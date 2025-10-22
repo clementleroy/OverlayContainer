8e3e20bee5aa351c07ad114fe18d31ac04edd2ce
Clément Leroy <clement.leroy@ratpsmartsystems.com>
2025-10-22 18:12:43 +0200

    Clean

--- Package.swift
+++ Package.swift
@@ -1,33 +1,23 @@
-// swift-tools-version:5.1
+// swift-tools-version: 5.10
 import PackageDescription
 
 let package = Package(
     name: "OverlayContainer",
     platforms: [
-        .iOS(.v10)
+        .iOS(.v15)
     ],
     products: [
         .library(
             name: "OverlayContainer",
             targets: ["OverlayContainer"]
-        ),
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/Quick/Nimble", from: "9.0.0"),
-        .package(url: "https://github.com/Quick/Quick", from: "4.0.0"),
     ],
     targets: [
         .target(
-            name: "OverlayContainer"
-        ),
-        .testTarget(
-            name: "OverlayContainerTests",
-            dependencies: [
-                "OverlayContainer",
-                "Nimble",
-                "Quick"
-            ]
+            name: "OverlayContainer",
+            path: "Source/OverlayContainer"
         )
-    ],
-    swiftLanguageVersions: [.v5]
+    ]
 )
